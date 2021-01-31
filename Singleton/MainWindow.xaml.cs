a8d88e3bc592ae740a7234e0d36bcfcf3ffdd031
Ondřej Novosád <novosad.ondrej.2017@skola.ssps.cz>
2021-01-31 18:27:43 +0100

    Add project files.

---
+++ Singleton/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Singleton
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_SaveClick(object sender, RoutedEventArgs e)
+        {
+            SingletonClass save = SingletonClass.Instance;
+            save.Pridat(name.Text,surename.Text,number.Text,date.Text);
+
+        }
+
+        private void Button_ShowClick(object sender, RoutedEventArgs e)
+        {
+            SingletonClass show = SingletonClass.Instance;
+            MessageBox.Show(show.Ukaz());
+
+        }
+    }
+}
---
+++ Singleton/SingletonClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class SingletonClass
+    {
+        //static List<string> database = new List<string>();
+        //private static readonly SingletonClass _instace = new SingletonClass();
+
+        static List<string> database;
+        private SingletonClass()
+        {
+            database = new List<string>();
+        }
+
+        static SingletonClass _instance = null;
+        private static readonly object locker = new object();
+
+        public static SingletonClass Instance
+        {
+            get 
+            {
+                lock (locker)
+                {
+                    if (_instance == null)
+                        _instance = new SingletonClass();
+                }
+                return _instance;
+            }
+        }
+
+        public void Pridat(string a, string b, string c, string d)
+        {
+            database.Add(a);
+            database.Add(b);
+            database.Add(c);
+            database.Add(d);
+        }
+
+        public string Ukaz()
+        {
+            return database.Count().ToString();
+        }
+    }
+}
